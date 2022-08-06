1b6a09b046cf1c150353d85a46876b0e97c9f326
Gustavo <gordono@unsa.edu.pe>
2022-08-05 20:34:36 -0500

    Added Encapsulation to Persona class

--- src/main/java/com/example/project/Persona.java
+++ src/main/java/com/example/project/Persona.java
@@ -1,10 +1,19 @@
 package com.example.project;
 
 public class Persona {
-    public String DNI, nombre;
+    private String DNI;
+    private String nombre;
 
     public Persona(String DNI, String nombre){
         this.DNI = DNI;
         this.nombre = nombre;
+    }
+
+    public String getDNI() {
+        return this.DNI;
+    }
+
+    public String getNombre() {
+        return this.nombre;
     }
 }
